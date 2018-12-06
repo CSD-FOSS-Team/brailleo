652c611e2921c8d4e7d06d8858de585e0fcaa6fd
Orestis Ousoultzoglou <oorestis@pm.me>
2018-12-06 22:22:45 +0200

    Update and rename test.py to getASCII.py

--- getASCII.py
+++ getASCII.py
@@ -1,3 +1,8 @@
+import getch
+
+#prints out braille characters's correspondant ASCII code equivalents
 string = "⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿"
 for char in string:
   print(ord(char))
+  
+  
