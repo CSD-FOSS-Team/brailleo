7fe4930bc22711b06a28a171c66bb63b20f0233c
xlxs4 <oorestis@csd.auth.gr>
2018-12-06 22:31:19 +0200

    add ASCII helper script

--- getASCII.py
+++ getASCII.py
@@ -4,5 +4,9 @@ import getch
 string = "⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿"
 for char in string:
   print(ord(char))
-  
-  
+print("every keypress will return it's correspondant ASCII code\npress esc to exit")
+while True:
+    key = ord(getch.getch())
+    print(key)
+    if key == 27:
+        break
--- links.md
+++ links.md
@@ -3,3 +3,5 @@
 ###### Braille ASCII
 [braille-ASCII]( https://en.wikipedia.org/wiki/Braille_ASCII)
 
+###### Representing and Displaying Braille
+[displaying-braille](https://dotlessbraille.org/displays.htm)
