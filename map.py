31ca786a4ab611b397a8acbc55779a1fefbe64fa
Orestis Ousoultzoglou <oorestis@pm.me>
2018-12-07 12:42:03 +0200

    map C string to Unicode
    see
    https://en.wikipedia.org/wiki/Braille_ASCII

---
+++ map.py
@@ -0,0 +1,7 @@
+intab = "todo"  
+outtab = "⠀⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿"
+transtab = str.maketrans(intab, outtab)
+
+#example
+strg = "todo"
+print(strg.translate(transtab))
